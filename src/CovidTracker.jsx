755e48a0046cacae78d814a56c89daa3a95bf3a0
rohank45 <kurane.rohan99@gmail.com>
2021-11-29 12:27:07 +0530

    final commit

--- src/CovidTracker.jsx
+++ src/CovidTracker.jsx
@@ -50,42 +50,33 @@ const CovidTracker = () => {
 
   return (
     <>
-      <Alert variant="danger text-center fw-bold">
+      <Alert variant="danger text-center fw-bold overflow-hidden">
         Refresh page for updates!
       </Alert>
 
       <div className="container mx-auto">
-        <div>
-          <header className="d-flex justify-content-center align-items-center">
-            <div
-              style={{ "max-width": "200px" }}
-              className="sm:visually-hidden"
-              ref={element}
-            ></div>
-            <p className="fw-bold display-6">COVID TRACKER</p>
-          </header>
-        </div>
+        <header className="d-flex justify-content-center align-items-center overflow-hidden">
+          <div style={{ "max-width": "200px" }} ref={element}></div>
+          <p className="fw-bold display-6">COVID TRACKER</p>
+        </header>
 
-        <div
-          className="
-            text-white fw-bold text-center my-5 text-uppercase d-xxl-flex"
-        >
+        <div className="text-white fw-bold my-5 text-uppercase responsiveCards overflow-hidden">
           <div
             className="card bg-primary mx-2 sm:col-6 overflow-hidden"
-            style={{ width: "18rem" }}
+            style={{ width: "17rem" }}
           >
             <div className="card-body">
               <p className="card-text">Active Cases</p>
               <p className="card-text display-5">{covidStats?.active_cases}</p>
             </div>
           </div>
-          <div className="card bg-warning mx-2" style={{ width: "18rem" }}>
+          <div className="card bg-warning mx-2" style={{ width: "17rem" }}>
             <div className="card-body">
               <p className="card-text">Total Cases</p>
               <p className="card-text display-5">{covidStats?.total_cases}</p>
             </div>
           </div>
-          <div className="card bg-danger mx-2" style={{ width: "18rem" }}>
+          <div className="card bg-danger mx-2" style={{ width: "17rem" }}>
             <div className="card-body">
               <p className="card-text">Total Deaths</p>
               <p className="card-text display-5">{covidStats?.total_deaths}</p>
@@ -100,50 +91,48 @@ const CovidTracker = () => {
             </div>
           </div>
         </div>
+
+        <div>
+          <table className="table table-hover text-uppercase rowScroll">
+            <thead className="bg-secondary text-white">
+              <tr>
+                <th scope="col">Country</th>
+                <th scope="col">active_cases</th>
+                <th scope="col">cases</th>
+                <th scope="col">deaths</th>
+                <th scope="col">total_recovered</th>
+                <th scope="col">total_tests</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              {covidData.map((stats, id) => {
+                const {
+                  active_cases,
+                  cases,
+                  country_name,
+                  deaths,
+                  total_recovered,
+                  total_tests,
+                  new_cases,
+                  new_deaths,
+                } = stats;
+
+                return (
+                  <tr key={id} className="text-uppercase fw-bold">
+                    <th scope="row">{country_name}</th>
+                    <td className="text-primary">{active_cases}</td>
+                    <td className="text-warning">{cases}</td>
+                    <td className="text-danger">{deaths}</td>
+                    <td className="text-success">{total_recovered}</td>
+                    <td className="text-info">{total_tests}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
-
-      <table className="table table-hover text-uppercase bg-secondary text-white overflow-scroll">
-        <thead>
-          <tr>
-            <th scope="col">Country</th>
-            <th scope="col">active_cases</th>
-            <th scope="col">cases</th>
-            <th scope="col">deaths</th>
-            <th scope="col">total_recovered</th>
-            <th scope="col">total_tests</th>
-          </tr>
-        </thead>
-      </table>
-
-      {covidData.map((stats, id) => {
-        const {
-          active_cases,
-          cases,
-          country_name,
-          deaths,
-          total_recovered,
-          total_tests,
-          new_cases,
-          new_deaths,
-        } = stats;
-
-        return (
-          <div key={id} className="text-uppercase fw-bold">
-            <table className="table table-hover overflow-scroll">
-              <tbody>
-                <tr>
-                  <th scope="row">{country_name}</th>
-                  <td className="text-primary">{active_cases}</td>
-                  <td className="text-warning">{cases}</td>
-                  <td className="text-danger">{deaths}</td>
-                  <td className="text-success">{total_recovered}</td>
-                  <td className="text-info">{total_tests}</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        );
-      })}
     </>
   );
 };
